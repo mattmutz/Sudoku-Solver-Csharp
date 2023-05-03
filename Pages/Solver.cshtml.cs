fb83b90dfc092dfc7d64dad55e95415c1abd2181
Matt Mutz <matt@mattmutz.com>
2023-05-03 16:05:05 -0500

    initial commit

---
+++ Pages/Solver.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Solvercs.Pages
+{
+    public class SolverModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Sudoku-Solver-Csharp
+
+## Credits
+[Puzzle board](https://gist.github.com/thebinarypenguin/4d45ffe87096e508800b5d11544bf2fa)
